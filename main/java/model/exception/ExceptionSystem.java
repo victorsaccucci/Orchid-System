63adebde18a3e115bb703af88a3f066b22e172f4
Victor Saccucci <victorsaccucci@hotmail.com>
2023-05-29 08:52:16 -0300

    Ajustes no POM

---
+++ main/java/model/exception/ExceptionSystem.java
@@ -0,0 +1,9 @@
+package model.exception;
+
+public class ExceptionSystem extends Exception{
+	private static final long serialVersionUID = -6002488916250531018L;
+	
+	public ExceptionSystem (String mensagem) {
+		super(mensagem);
+	}
+}
